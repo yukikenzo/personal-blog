e4ee874bb6ff3970c31d77a0823410228528b459
yukikenzo <gentlejp1122@gmail.com>
2023-11-08 09:45:07 -0300

    Feat: model and migration on categoria

---
+++ app/Models/Categoria.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\posts\Post;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
+}
--- app/Models/posts/Post.php
+++ app/Models/posts/Post.php
@@ -2,10 +2,15 @@
 
 namespace App\Models\posts;
 
+use App\Models\Categoria;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function categoria() {
+        return $this->belongsTo(Categoria::class);
+    }
 }
